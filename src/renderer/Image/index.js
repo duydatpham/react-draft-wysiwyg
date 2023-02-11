47321e56f45b25fed0a0a8a044132b6482944d93
Duy Đạt Phạm <duydatpham@gmail.com>
2023-02-11 10:20:37 +0700

    #update

--- src/renderer/Image/index.js
+++ src/renderer/Image/index.js
@@ -57,7 +57,7 @@ const getImageComponent = config => class Image extends Component {
           },
         )}
       >
-        <Option
+        {/* <Option
           onClick={this.setEntityAlignmentLeft}
           className="rdw-image-alignment-option"
         >
@@ -74,12 +74,12 @@ const getImageComponent = config => class Image extends Component {
           className="rdw-image-alignment-option"
         >
           Right
-        </Option>
+        </Option> */}
         <Option
           onClick={this.setEntityAlignmentRight}
           className="rdw-image-alignment-option"
         >
-          <svg width="24" height="24" viewBox="0 0 24 24" role="presentation"><path d="M7 7h10a1 1 0 010 2H7a1 1 0 110-2zm2.78 11a1 1 0 01-.97-.757L7.156 10.62A.5.5 0 017.64 10h8.72a.5.5 0 01.485.621l-1.656 6.622a1 1 0 01-.97.757H9.781zM11 6h2a1 1 0 011 1h-4a1 1 0 011-1z" fill="currentColor" fill-rule="evenodd"></path></svg>
+          <span style={{ color: 'red' }} >Xoá</span>
         </Option>
       </div>
     );
